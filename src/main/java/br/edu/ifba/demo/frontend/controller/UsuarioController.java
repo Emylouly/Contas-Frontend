fc0924c514b7fe010f9c6d9d69410195869edb5b
Emylouly <emilylouisemika@gmail.com>
2025-03-19 15:46:15 -0300

    criar

--- src/main/java/br/edu/ifba/demo/frontend/controller/UsuarioController.java
+++ src/main/java/br/edu/ifba/demo/frontend/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -38,6 +39,11 @@ public class UsuarioController {
     public String deletarUsuario(@PathVariable Long id, RedirectAttributes redirectAttributes) {
         usuarioService.deletarUsuarios(id, redirectAttributes);
         return "redirect:/usuario/listall";
+    }
+
+    @GetMapping("/form")
+    public ModelAndView formularioUsuario() {
+        return new ModelAndView("usuario_form");
     }
 
 }
